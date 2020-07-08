66440e755c94e29f64babfdaeb6dd19f0781620e
Ben Fung <benfung1996@gmail.com>
2020-07-07 17:30:47 -0700

    Update server.js

--- server.js
+++ server.js
@@ -65,6 +65,7 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 app.use("/", users);
+app.use(cors());
 
 const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 server.listen(port, () => console.log(`Server up and running on port ${port} !`));
